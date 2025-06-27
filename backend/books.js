92eb9a44a5241eaddf707362ea8bfe10851e599d
AYESHA MALAK <ayeshamalak34@gmail.com>
2025-06-27 22:41:34 +0500

    Add files via upload

---
+++ backend/books.js
@@ -0,0 +1,54 @@
+
+
+const books = [
+  {
+    id: 1,
+    title: "Atomic Habits",
+    author: "James Clear",
+    category: "Self-Help",
+    price: 999,
+    image: "/images/atomic-habits.jpg"
+  },
+  {
+    id: 2,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    category: "Fiction",
+    price: 799,
+    image: "/images/The AL-Chemist.jpg"
+  },
+  {
+    id: 3,
+    title: "Deep Work",
+    author: "Cal Newport",
+    category: "Productivity",
+    price: 1199,
+    image: "/images/deep-work.jpg"
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    category: "Fantasy",
+    price: 899,
+    image: "/images/harry-potter.jpg"
+  },
+  {
+    id: 5,
+    title: "Sapiens: A Brief History of Humankind",
+    author: "Yuval Noah Harari",
+    category: "History",
+    price: 1499,
+    image: "/images/Sapiens.jpg"
+  },
+  {
+    id: 6,
+    title: "Start With Why",
+    author: "Simon Sinek",
+    category: "Motivation",
+    price: 999,
+    image: "/images/start-with-why.jpg"
+  }
+];
+
+module.exports = books;
